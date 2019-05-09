01563d011f22f5da1eab3394a6a62f144d5d58e4
sabelogumede <iamsabelogumede@gmail.com>
2019-05-09 10:52:38 +0200

    single-file-components

---
+++ src/components/BigYellowUsername.vue
@@ -0,0 +1,21 @@
+<template>
+    <p class="username">{{username}}</p>
+</template>
+
+<script>
+    export default {
+        props: {
+            username: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .username {
+        font-size: 50px;
+        color: #DCAE1D;
+    }
+</style>
